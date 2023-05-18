bcb0f8c3d12c310d998fff603d053aa872fd1470
Abiola Martins <hsemartins@gmail.com>
2023-05-18 11:29:32 +0300

    modified:   src/App.js         modified:   src/components/Expenses/ExpenseItem.js         new file:   src/components/NewExpense/ExpenseForm.css         new file:   src/components/NewExpense/ExpenseForm.js         new file:   src/components/NewExpense/NewExpense.css         new file:   src/components/NewExpense/NewExpense.js

--- expenses/src/components/Expenses/ExpenseItem.js
+++ expenses/src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
   const clickHandler = () => {
-    console.log("Have been Clicked just Now!!!");
+    setTitle("Updated now!!!");
+    console.log(title);
   };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
         <button onClick={clickHandler}>Clik to Change Title</button>
       </div>
